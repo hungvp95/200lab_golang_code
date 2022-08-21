dc062c50f9c242d6553ee75997d26654ff7b0ec0
daiduong.workmail <daiduong.workmail@gmail.com>
2022-08-21 23:03:09 +0700

    add crud example gorm

--- simple-crud-backend/main.go
+++ simple-crud-backend/main.go
@@ -4,30 +4,67 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type Note struct {
-	Id      int
-	Title   string
-	Content string
+	Id      int    `gorm:"primary_key;column:id;"`
+	Title   string `json:"title" gorm:"not null;column:title;"`
+	Content string `json:"content" gorm:"not null;column:content;"`
+}
+
+type NoteUpdate struct {
+	Title   *string
+	Content *string
+}
+
+func (Note) TableName() string {
+	return "notes"
 }
 
 func Main() {
 	dsn := os.Getenv("DBConnectionStr")
-
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	/// CREATE
 	note := Note{
-		Title: "Hello World",
-		Content: "Getting started with golang",
+		Title:   "This is a title",
+		Content: "This is a content",
 	}
 	db.Create(&note)
-	
-	fmt.Printf("%+v\n", note)
+
+	fmt.Println("Start reading")
+	time.Sleep(time.Second * 10)
+
+	/// READ
+	readNote := Note{}
+	db.Where("title = ?", "This is a title").First(&readNote)
+	fmt.Printf("Read note : %+v\n", readNote)
+
+	fmt.Println("Start updating")
+	time.Sleep(time.Second * 10)
+
+	/// UPDATE
+	newTitle, newContent := "This is a new title", ""
+	db. //
+		Table(Note{}.TableName()).
+		Where("id = ?", readNote.Id).
+		Updates(NoteUpdate{
+			Title:   &newTitle,
+			Content: &newContent,
+		})
+	db.Where("id = ?", readNote.Id).First(&readNote)
+	fmt.Printf("Read note after update : %+v\n", readNote)
+
+	fmt.Println("Start deleting")
+	time.Sleep(time.Second * 10)
+
+	/// DELETE
+	db.Table(Note{}.TableName()).Where("id = ?", readNote.Id).Delete(nil)
 }
